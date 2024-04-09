44c84fc4f57f13fa3c65ced401eaf32595165426
PearceStrickland <78804764+PearceStrickland@users.noreply.github.com>
2024-04-09 15:27:21 -0400

    Navigating set up

--- attorney-website/src/GameScreen.js
+++ attorney-website/src/GameScreen.js
@@ -2,13 +2,7 @@ import React from 'react';
 
 function GameScreen() {
   return (
-      <ul>
-         <li>Instruction 1: Read the case carefully and gather all relevant information.</li>
-         <li>Instruction 2: Analyze the evidence and identify key points to support your argument.</li>
-         <li>Instruction 3: Prepare your opening statement and anticipate counterarguments.</li>
-         <li>Instruction 4: Cross-examine witnesses and present your case persuasively.</li>
-         <li>Instruction 5: Deliver a compelling closing argument to sway the jury.</li>
-     </ul>
+      <h1>Test</h1>
 
   );
 }
--- attorney-website/src/HomeScreen.js
+++ attorney-website/src/HomeScreen.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import './App.css';
+import { useNavigate } from 'react-router-dom';
+import './HomeScreen.css';
 
 function HomeScreen() {
+  let navigate = useNavigate(); // Hook to get the navigate function
+
+  function handleProceed() {
+    navigate('/game'); // Use navigate function to change the route
+  }
+
   return (
     <div className="app">
       <div className="background-image"></div>
@@ -14,7 +21,7 @@ function HomeScreen() {
           <li>Instruction 4: Cross-examine witnesses and present your case persuasively.</li>
           <li>Instruction 5: Deliver a compelling closing argument to sway the jury.</li>
         </ul>
-        <button className="proceed-button">Proceed to the Game</button>
+        <button className="proceed-button" onClick={handleProceed}>Proceed to the Game</button>
       </div>
     </div>
   );
